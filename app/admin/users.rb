a6253cad569de42765ebcbbcc7d6e754abbffcce
Kevin Morton <kevin@kjproductions.com>
2018-07-30 00:34:48 -0400

    Adding admin code for downloading Winners list in a CSV file

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,14 +1,43 @@
-# Export to CSV with the referrer_id
+require 'csv'
+
 ActiveAdmin.register User do
   csv do
-    column :id
-    column :email
-    column :referral_code
-    column :referrer_id
-    column :created_at
-    column :updated_at
+   column :id
+   column :email
+   column :referral_code
+   column :referrer_id
+   column :created_at
+   column :updated_at
   end
 
   actions :index, :show
 
+  action_item do
+    link_to "Download Winners", winners_admin_users_path
+  end
+
+  collection_action :winners, method: :get do
+    stops = User::REFERRAL_STEPS.map{|stop| stop["count"]}
+
+   winners = Hash.new {|h,k| h[k]=[]}
+   User.all.each do |user|
+      found = nil
+
+    stops.reverse_each do |stop|
+      found = stop if stop <= user.referrals.count and !found
+    end
+
+   winners[found] << user if found
+  end
+
+  winners = CSV.generate do |csv|
+   winners.each do |stop, list|
+    csv << [stop, "------"]
+     list.each do |user|
+      csv << [user.email, user.referrals.count]
+     end
+    end
+   end
+   send_data winners, filename: "winners.csv"
+  end
 end
